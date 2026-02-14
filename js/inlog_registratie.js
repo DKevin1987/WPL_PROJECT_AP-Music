36b6119202e997b1e240a278e8e52baa31dfabe2
Kevin Dom <s167175@ap.be>
2026-02-14 19:59:25 +0100

    inlog java_01

--- js/inlog_registratie.js
+++ js/inlog_registratie.js
@@ -1,7 +1,26 @@
+"use strict";
+
+// Pakt het inlog en registratie form element vast.
+
 const inlogformEl = document.querySelector("#inlogform");
 const registrationformEl = document.querySelector("#registrationform");
 
-console.log(inlogformEl);
-console.log(registrationformEl);
+
+// GEDEELTE VOOR INLOG-PAGINA
+const userEmailEl = inlogformEl.querySelector("#useremail");
+const userPaswordEl = inlogformEl.querySelector("#userpasword");
+const inlogButtonEl = inlogformEl.querySelector("#inlogButton");
+let userEmailVal;
+let userPaswordVal;
+
+inlogButtonEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("klik");
+  userEmailVal = inlogformEl.querySelector("#useremail").value;
+  userPaswordVal = inlogformEl.querySelector("#userpasword").value;
+  console.log(userEmailVal);
+  console.log(userPaswordVal);
+});
 
 
+// GEDEELTE VOOR REGISTRATIE-PAGINA
