cbc401a4ade8fc95094210fe6b0cee5dad992038
Kevin Dom <s167175@ap.be>
2026-02-14 15:54:07 +0100

    CSS inlog en registratie desktop en mobile versie_01

---
+++ js/inlog_registratie.js
@@ -0,0 +1,7 @@
+const inlogformEl = document.querySelector("#inlogform");
+const registrationformEl = document.querySelector("#registrationform");
+
+console.log(inlogformEl);
+console.log(registrationformEl);
+
+
