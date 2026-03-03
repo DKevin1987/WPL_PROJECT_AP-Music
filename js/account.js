cb5300a211d34cca4f09c80d41094b14373d02d8
KyaroVG <kyarorebecca@hotmail.com>
2026-03-03 17:56:48 +0100

    eerste deel accountPagina klaar

---
+++ js/account.js
@@ -0,0 +1,58 @@
+// knoppen
+const gebruikersnaamBtn = document.querySelector(".Btn-naam");
+const icoonBtn = document.querySelector(".Btn-icoon");
+const beveiliginBtn = document.querySelector(".Btn-beveiliging");
+const moodBtn = document.querySelector(".Btn-mood");
+// containers
+const gebruikersnaamContainer = document.getElementById(
+  "gebruikersnaam-settings",
+);
+const icoonContainer = document.getElementById("icoon-settings");
+const beveiligingContainer = document.getElementById("beveiliging-settings");
+const moodContainer = document.getElementById("mood-settings");
+
+//code
+gebruikersnaamBtn.addEventListener("click", () => {
+  gebruikersnaamContainer.classList.remove("hidden");
+  icoonContainer.classList.add("hidden");
+  beveiligingContainer.classList.add("hidden");
+  moodContainer.classList.add("hidden");
+});
+icoonBtn.addEventListener("click", () => {
+  gebruikersnaamContainer.classList.add("hidden");
+  icoonContainer.classList.remove("hidden");
+  beveiligingContainer.classList.add("hidden");
+  moodContainer.classList.add("hidden");
+});
+beveiliginBtn.addEventListener("click", () => {
+  gebruikersnaamContainer.classList.add("hidden");
+  icoonContainer.classList.add("hidden");
+  beveiligingContainer.classList.remove("hidden");
+  moodContainer.classList.add("hidden");
+});
+moodBtn.addEventListener("click", () => {
+  gebruikersnaamContainer.classList.add("hidden");
+  icoonContainer.classList.add("hidden");
+  beveiligingContainer.classList.add("hidden");
+  moodContainer.classList.remove("hidden");
+});
+
+// code icoonen
+const userIcoon = document.getElementById("accountIcon");
+const standaardUser = document.getElementById("userIcoonStandaard");
+const user1 = document.getElementById("userIcoon1");
+const user2 = document.getElementById("userIcoon2");
+const user3 = document.getElementById("userIcoon3");
+
+standaardUser.addEventListener("click", () => {
+  userIcoon.src = "./assets/icons/person.jpg";
+});
+user1.addEventListener("click", () => {
+  userIcoon.src = "./assets/gebruikers-icoon/user1.png";
+});
+user2.addEventListener("click", () => {
+  userIcoon.src = "./assets/gebruikers-icoon/user2.png";
+});
+user3.addEventListener("click", () => {
+  userIcoon.src = "./assets/gebruikers-icoon/user3.png";
+});
