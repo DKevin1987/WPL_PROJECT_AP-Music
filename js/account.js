a6552dc79ee502658aaebfe855d92a63cd1102f8
KyaroVG <kyarorebecca@hotmail.com>
2026-03-10 17:54:00 +0100

    icoons uniform gemaakt en mood - user icoonen opgeslagen gehouden

--- js/account.js
+++ js/account.js
@@ -37,42 +37,116 @@ moodBtn.addEventListener("click", () => {
   moodContainer.classList.remove("hidden");
 });
 
-// code icoonen
+// // code icoonen
+// const userIcoon = document.getElementById("accountIcon");
+// const standaardUser = document.getElementById("userIcoonStandaard");
+// const user1 = document.getElementById("userIcoon1");
+// const user2 = document.getElementById("userIcoon2");
+// const user3 = document.getElementById("userIcoon3");
+
+// standaardUser.addEventListener("click", () => {
+//   userIcoon.src = "./assets/icons/person.jpg";
+// });
+// user1.addEventListener("click", () => {
+//   userIcoon.src = "./assets/gebruikers-icoon/user1.png";
+// });
+// user2.addEventListener("click", () => {
+//   userIcoon.src = "./assets/gebruikers-icoon/user2.png";
+// });
+// user3.addEventListener("click", () => {
+//   userIcoon.src = "./assets/gebruikers-icoon/user3.png";
+// });
+
+// // code mood
+// const moodIcoon = document.getElementById("moodIcon");
+// const standaardMood = document.getElementById("moodIcoonStandaard");
+// const mood1 = document.getElementById("moodIcoonHappy");
+// const mood2 = document.getElementById("moodIcoonSad");
+// const mood3 = document.getElementById("moodIcoonMad");
+
+// standaardMood.addEventListener("click", () => {
+//   moodIcoon.src = "./assets/icons/moods/neutral.png";
+// });
+// mood1.addEventListener("click", () => {
+//   moodIcoon.src = "./assets/icons/moods/smile.png";
+// });
+// mood2.addEventListener("click", () => {
+//   moodIcoon.src = "./assets/icons/moods/sad.png";
+// });
+// mood3.addEventListener("click", () => {
+//   moodIcoon.src = "./assets/icons/moods/angry.png";
+// });
+
+// moods en user icons
 const userIcoon = document.getElementById("accountIcon");
-const standaardUser = document.getElementById("userIcoonStandaard");
-const user1 = document.getElementById("userIcoon1");
-const user2 = document.getElementById("userIcoon2");
-const user3 = document.getElementById("userIcoon3");
-
-standaardUser.addEventListener("click", () => {
-  userIcoon.src = "./assets/icons/person.jpg";
-});
-user1.addEventListener("click", () => {
-  userIcoon.src = "./assets/gebruikers-icoon/user1.png";
-});
-user2.addEventListener("click", () => {
-  userIcoon.src = "./assets/gebruikers-icoon/user2.png";
-});
-user3.addEventListener("click", () => {
-  userIcoon.src = "./assets/gebruikers-icoon/user3.png";
-});
-
-// code mood
 const moodIcoon = document.getElementById("moodIcon");
-const standaardMood = document.getElementById("moodIcoonStandaard");
-const mood1 = document.getElementById("moodIcoonHappy");
-const mood2 = document.getElementById("moodIcoonSad");
-const mood3 = document.getElementById("moodIcoonMad");
 
-standaardMood.addEventListener("click", () => {
-  moodIcoon.src = "./assets/icons/moods/neutral.png";
+// user icon opslaan
+window.addEventListener("DOMContentLoaded", () => {
+  const savedUser = localStorage.getItem("gekozenUserIcoon");
+  const savedMood = localStorage.getItem("gekozenMoodIcoon");
+
+  if (savedUser && userIcoon) {
+    userIcoon.src = savedUser;
+  }
+  if (savedMood && moodIcoon) {
+    moodIcoon.src = savedMood;
+  }
 });
-mood1.addEventListener("click", () => {
-  moodIcoon.src = "./assets/icons/moods/smile.png";
+
+document.getElementById("userIcoonStandaard")?.addEventListener("click", () => {
+  const pad = "./assets/icons/person.jpg";
+  userIcoon.src = pad;
+  localStorage.setItem("gekozenUserIcoon", pad);
 });
-mood2.addEventListener("click", () => {
-  moodIcoon.src = "./assets/icons/moods/sad.png";
+
+document.getElementById("userIcoon1")?.addEventListener("click", () => {
+  const pad = "./assets/gebruikers-icoon/user1.png";
+  userIcoon.src = pad;
+  localStorage.setItem("gekozenUserIcoon", pad);
 });
-mood3.addEventListener("click", () => {
-  moodIcoon.src = "./assets/icons/moods/angry.png";
+
+document.getElementById("userIcoon2")?.addEventListener("click", () => {
+  const pad = "./assets/gebruikers-icoon/user2.png";
+  userIcoon.src = pad;
+  localStorage.setItem("gekozenUserIcoon", pad);
 });
+
+document.getElementById("userIcoon3")?.addEventListener("click", () => {
+  const pad = "./assets/gebruikers-icoon/user3.png";
+  userIcoon.src = pad;
+  localStorage.setItem("gekozenUserIcoon", pad);
+});
+
+document.getElementById("moodIcoonStandaard")?.addEventListener("click", () => {
+  const pad = "./assets/icons/moods/neutral.png";
+  moodIcoon.src = pad;
+  localStorage.setItem("gekozenMoodIcoon", pad);
+});
+
+// Mood opslaan
+function updateMood(pad) {
+  if (moodIcoon) {
+    moodIcoon.src = pad;
+    localStorage.setItem("gekozenMoodIcoon", pad);
+  }
+}
+
+document
+  .getElementById("moodIcoonStandaard")
+  .addEventListener("click", () =>
+    updateMood("./assets/icons/moods/neutral.png"),
+  );
+document
+  .getElementById("moodIcoonHappy")
+  .addEventListener("click", () =>
+    updateMood("./assets/icons/moods/smile.png"),
+  );
+document
+  .getElementById("moodIcoonSad")
+  .addEventListener("click", () => updateMood("./assets/icons/moods/sad.png"));
+document
+  .getElementById("moodIcoonMad")
+  .addEventListener("click", () =>
+    updateMood("./assets/icons/moods/angry.png"),
+  );
--- js/generalJava.js
+++ js/generalJava.js
@@ -8,6 +8,16 @@ const dropdownMenu = document.getElementById("dropdownMenu");
 //  MENU DROPDOWN BUTTON
 //
 menuBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    dropdownMenu.classList.toggle("hidden");
+  event.preventDefault();
+  dropdownMenu.classList.toggle("hidden");
+});
+
+// mood opgeslagen houden
+window.addEventListener("DOMContentLoaded", () => {
+  const moodIcoon = document.getElementById("moodIcon");
+  const savedMood = localStorage.getItem("gekozenMoodIcoon");
+
+  if (savedMood && moodIcoon) {
+    moodIcoon.src = savedMood;
+  }
 });
