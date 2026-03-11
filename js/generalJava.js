187b01d0be13bb136eb71aff59131d9f6c0ba8fd
Kevin Dom <s167175@ap.be>
2026-03-11 20:58:59 +0100

    homepage-test alle buttons mobile klaar

--- js/generalJava.js
+++ js/generalJava.js
@@ -11,3 +11,17 @@ menuBtn.addEventListener("click", (event) => {
     event.preventDefault();
     dropdownMenu.classList.toggle("hidden");
 });
+
+const searchBtn = document.querySelector(".search-button");
+const searchForm = document.querySelector(".header-search");
+const searchFormBtn = document.querySelector(".search-button2");
+
+searchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    searchForm.classList.remove("hidden");
+});
+
+searchFormBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.assign("/searchpage.html");
+});
