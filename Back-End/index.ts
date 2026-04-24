f99c447a09ffb2ea935ab7c2357df8b655d3013e
KyaroVG <kyarorebecca@hotmail.com>
2026-04-24 18:14:16 +0000

    route voor detail pagina aangemaakt

--- Back-End/index.ts
+++ Back-End/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import homeRouter from "./routes/homeRoute";
+import detailRouter from "./routes/detailRoute";
 
 dotenv.config();
 
@@ -16,14 +17,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
-	res.render("index", {
-		title: "Hello World",
-		message: "Hello World",
-	});
+  res.render("index", {
+    title: "Hello World",
+    message: "Hello World",
+  });
 });
 
 app.use("/home", homeRouter());
+app.use("/detail", detailRouter());
 
 app.listen(app.get("port"), () => {
-	console.log("Server started on http://localhost:" + app.get("port"));
+  console.log("Server started on http://localhost:" + app.get("port"));
 });
