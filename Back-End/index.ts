a14ad4577093ee406a769a3426e32a5962e4fea1
Opong Nana <s169435@ap.be>
2026-04-27 18:06:06 +0000

    game_page op backend voor nu nog ni dynamisch

--- Back-End/index.ts
+++ Back-End/index.ts
@@ -2,6 +2,10 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
 
+import vergelijkRouter from "./routes/vergelijkRouter";
+import gameRouter from "./routes/gameRouter";
+import playlistRouter from "./routes/playlistRouter";
+
 dotenv.config();
 
 const app : Express = express();
@@ -10,6 +14,11 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/vergelijk", vergelijkRouter);
+app.use("/game", gameRouter);
+app.use("/playlist", playlistRouter);
+
 app.set('views', path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
